32550074a0217d371c7c9b6832b1cf2df3be4d8d
Miguel Boulette <mboulette@idealtechnology.net>
2017-10-23 20:15:37 -0400

    Ajouter les connexion Square

--- src/config/square.php
+++ src/config/square.php
@@ -1,5 +1,23 @@
 <?php
 
+
+/* MY SANDBOX */
+/* 
 define('square_applicationId', 'sandbox-sq0idp-BILeMjJoV8uP7c1jEmuoVA');
 define('square_location_id','CBASEJUrj3bfWmCFo87mD33DyKkgAQ');
 define('square_access_token','sandbox-sq0atb-TswDh3iGpRqWZNH7WbQLZA');
+*/
+
+/*ABEAS SANDBOX */
+define('square_applicationId', 'sandbox-sq0idp-fpkZGjVt625z7iNo3C1Eeg');
+define('square_location_id','CBASEHBDNsEbnXXa9Zjb0YtAXLcgAQ');
+define('square_access_token','sandbox-sq0atb-sFMhETztNbWaJcTB0j4PQA');
+
+
+/*ABEAS LIVE */
+/* 
+define('square_applicationId', 'sq0idp-fpkZGjVt625z7iNo3C1Eeg');
+define('square_location_id','91P14P0CM1MSE');
+define('square_access_token','sq0atp-bvDFyRQ8Rkabx5uWhPfXkQ');
+*/
+
