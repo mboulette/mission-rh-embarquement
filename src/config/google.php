8b822fe29587a245397ad8ada0fbebd4e018e6e1
Miguel Boulette <mboulette@idealtechnology.net>
2019-03-13 19:05:10 -0400

    Correction Connexion Google

--- src/config/google.php
+++ src/config/google.php
@@ -1,9 +1,16 @@
 <?php
 
 //Google
-
+/*
 define('GOOGLE_CLIENT_ID','290147044849-hakc5kt3gb3vcsk7ml70fn1gse399uba.apps.googleusercontent.com');
 define('GOOGLE_CLIENT_SECRET','taFE_ew_4cD2PWvC8ioProwF');
+define('GOOGLE_REDIRECT_URI', $GLOBALS['app_url'].'/auth?action=google');
+define('GOOGLE_APPROVAL_PROMPT','auto');
+define('GOOGLE_ACCESS_TYPE','offline');
+*/
+
+define('GOOGLE_CLIENT_ID','381509406668-vhvvm85ed5agf5f8ovp7f18f1g6fmu42.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET','zMD4hnIbTSAQIBU4VStyMlVo');
 define('GOOGLE_REDIRECT_URI', $GLOBALS['app_url'].'/auth?action=google');
 define('GOOGLE_APPROVAL_PROMPT','auto');
 define('GOOGLE_ACCESS_TYPE','offline');
